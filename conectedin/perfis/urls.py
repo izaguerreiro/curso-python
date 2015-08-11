d14830ff894592064da7768e2c868efea4cc4fa7
Izabela Guerreiro <izabelamaxlider@gmail.com>
2015-08-11 17:13:12 -0300

    fix

---
+++ conectedin/perfis/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'conectedin.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^$', perfis.views.index),
+    url(r'^perfis/(?P<perfil_id>\d+)$', perfis.views.exibir),
+)
+
---
+++ conectedin/perfis/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+from perfis.models import Perfil
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def exibir(request, perfil_id):
+	perfil = Perfil()
+
+    if perfil_id == '1':
+        perfil = Perfil('Izabela Guerreiro', 'izaguerreiro@gmail.com'.  '222222', 'Izepa')
+
+    if perfil_id == '2':
+        perfil = Perfil('Paulo Roberto', 'paulo.pinda@gmail.com'.  '2222', 'Izepa')
+
+
+    return render(request, 'perfil.html', {'perfil': perfil})
