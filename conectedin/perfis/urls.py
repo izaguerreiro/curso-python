eb695ed654acc14c3e85cdf0f8f31998607768aa
Izabela Guerreiro <izaguerreiro@gmail.com>
2015-08-11 23:07:01 -0300

    fix

--- conectedin/perfis/urls.py
+++ conectedin/perfis/urls.py
@@ -1,12 +1,11 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'conectedin.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', perfis.views.index),
-    url(r'^perfis/(?P<perfil_id>\d+)$', perfis.views.exibir),
+    url(r'^$', 'perfis.views.index'),
+    url(r'^perfis/(?P<perfil_id>\d+)$', 'perfis.views.exibir'),
 )
 
--- conectedin/perfis/views.py
+++ conectedin/perfis/views.py
@@ -10,11 +10,10 @@ def index(request):
 def exibir(request, perfil_id):
 	perfil = Perfil()
 
-    if perfil_id == '1':
-        perfil = Perfil('Izabela Guerreiro', 'izaguerreiro@gmail.com'.  '222222', 'Izepa')
+	if perfil_id == '1':
+		perfil = Perfil('Izabela Guerreiro', 'izaguerreiro@gmail.com',  '222222', 'Izepa')
 
-    if perfil_id == '2':
-        perfil = Perfil('Paulo Roberto', 'paulo.pinda@gmail.com'.  '2222', 'Izepa')
+	if perfil_id == '2':
+		perfil = Perfil('Paulo Roberto', 'paulo.pinda@gmail.com',  '2222', 'Izepa')
 
-
-    return render(request, 'perfil.html', {'perfil': perfil})
+	return render(request, 'perfil.html', {'perfil': perfil})
